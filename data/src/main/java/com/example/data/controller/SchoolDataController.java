c9eb253fbde4095277f5b2243006444b1b2b8958
jdegand <70610011+jdegand@users.noreply.github.com>
2024-07-06 15:39:16 -0400

    fix: format and remove code from school data controller

--- data/src/main/java/com/example/data/controller/SchoolDataController.java
+++ data/src/main/java/com/example/data/controller/SchoolDataController.java
@@ -34,25 +34,6 @@ public class SchoolDataController {
 		return schoolData;
 	}
 
-	/*
-	 * @PostMapping(path = "/addWebhookEvent/{schoolId}")
-	 * public String addWebhookEvent(@PathVariable Integer schoolId,
-	 * 
-	 * @RequestBody WebhookDetails webhookDetails) {
-	 * Optional<SchoolData> schoolData = schoolDataRepository.findById(schoolId);
-	 * 
-	 * WebhookDetails details = new WebhookDetails();
-	 * details.setEventName(webhookDetails.getEventName());
-	 * details.setEndPointUrl(webhookDetails.getEndPointUrl());
-	 * 
-	 * details.setSchoolData(schoolData.get());
-	 * 
-	 * schoolData.get().getWebhookDetails().add(details);
-	 * schoolDataRepository.save(schoolData.get());
-	 * return "Webhook Added";
-	 * }
-	 */
-
 	@PostMapping(path = "/addWebhookEvent/{schoolId}")
 	public String addWebhookEvent(@PathVariable Integer schoolId,
 			@RequestBody WebhookDetails webhookDetails) {
@@ -90,20 +71,21 @@ public class SchoolDataController {
 				.findFirst()
 				.orElse(null);
 
-		if (webhookDetails != null && webhookDetails.getEndPointUrl() != null) { // should do a better job validating URL
+		if (webhookDetails != null && webhookDetails.getEndPointUrl() != null) { // should do a better job validating
+																					// URL
 			String url = webhookDetails.getEndPointUrl(); // localhost:9000/
 			url += "/" + reqData.getName();
 			RestTemplate restTemplate = new RestTemplate();
-			//String result = restTemplate.getForObject(url, String.class);
+			// String result = restTemplate.getForObject(url, String.class);
 
 			RestClient restClient = RestClient.create(restTemplate);
-			
+
 			ResponseEntity<String> response = restClient
-			.get()
-			.uri(url)
-			.retrieve()
-			.toEntity(String.class);
-			 
+					.get()
+					.uri(url)
+					.retrieve()
+					.toEntity(String.class);
+
 			String result = response.getBody();
 
 			System.out.println("result: " + result);
@@ -112,118 +94,3 @@ public class SchoolDataController {
 	}
 
 }
-
-/*
- * package com.example.data.controller;
- * 
- * import java.util.ArrayList;
- * import java.util.List;
- * import java.util.Optional;
- * 
- * import org.springframework.beans.factory.annotation.Autowired;
- * import org.springframework.web.bind.annotation.PathVariable;
- * import org.springframework.web.bind.annotation.PostMapping;
- * import org.springframework.web.bind.annotation.RequestBody;
- * import org.springframework.web.bind.annotation.RequestMapping;
- * import org.springframework.web.bind.annotation.RequestParam;
- * import org.springframework.web.bind.annotation.RestController;
- * import org.springframework.web.client.RestTemplate;
- * 
- * import com.example.data.model.SchoolData;
- * import com.example.data.model.Student;
- * import com.example.data.model.WebhookDetails;
- * import com.example.data.repository.SchoolDataRepository;
- * 
- * @RestController
- * 
- * @RequestMapping("/schools")
- * public class SchoolDataController {
- * 
- * @Autowired
- * private SchoolDataRepository schoolDataRepository;
- * 
- * @PostMapping(path = "/addNewSchool")
- * public SchoolData addNewSchool(@RequestParam String schoolName) {
- * SchoolData schoolData = new SchoolData();
- * schoolData.setSchoolName(schoolName);
- * schoolDataRepository.save(schoolData);
- * return schoolData;
- * }
- * 
- * @PostMapping(path = "/addWebhookEvent/{schoolId}")
- * public String addWebhookEvent(@PathVariable Integer schoolId,
- * 
- * @RequestBody WebhookDetails webhookDetails) {
- * Optional<SchoolData> schoolData = schoolDataRepository.findById(schoolId);
- * 
- * List<WebhookDetails> webhooks = new ArrayList<WebhookDetails>();
- * WebhookDetails details = new WebhookDetails();
- * details.setEventName(webhookDetails.getEventName());
- * details.setEndPointUrl(webhookDetails.getEndPointUrl());
- * 
- * details.setSchoolData(schoolData.get());
- * webhooks.add(details);
- * 
- * schoolData.get().setWebhookDetails(webhooks);
- * schoolDataRepository.save(schoolData.get());
- * return "Webhook Added";
- * }
- * 
- * @PostMapping(path = "/addStudent/{schoolId}")
- * public String addStudent(@PathVariable Integer schoolId, @RequestBody Student
- * reqData) {
- * Optional<SchoolData> schoolData = schoolDataRepository.findById(schoolId);
- * SchoolData schoolData2 = schoolData.get();
- * 
- * List<Student> students = new ArrayList<Student>(); // creating new ArrayList
- * here will be problematic
- * Student student = new Student();
- * student.setAge(reqData.getAge());
- * student.setName(reqData.getName());
- * student.setSchoolData(schoolData2);
- * students.add(student);
- * 
- * schoolData2.setStudents(students);
- * schoolDataRepository.save(schoolData2);
- * /*
- * 
- * // This didn't work because eventData.getEventName() can be null
- * // I swapped order of the check and to get it to work
- * 
- * WebhookDetails webhookDetails = schoolData2.getWebhookDetails().stream()
- * .filter(eventData -> (eventData.getEventName().equals("add")))
- * .findFirst()
- * .orElse(null);
- *
- * 
- * WebhookDetails webhookDetails = schoolData2.getWebhookDetails().stream()
- * .filter(eventData -> "add".equals(eventData.getEventName()))
- * .findFirst()
- * .orElse(null);
- * 
- * if (webhookDetails != null && webhookDetails.getEndPointUrl() != null) {
- * String url = webhookDetails.getEndPointUrl(); // localhost:9000/
- * url += "/" + reqData.getName();
- * RestTemplate restTemplate = new RestTemplate();
- * String result = restTemplate.getForObject(url, String.class);
- * 
- * /*
- * RestClient restClient = RestClient.create(restTemplate);
- * 
- * ResponseEntity<String> response = restClient
- * .get()
- * .uri(url)
- * .retrieve()
- * .toEntity(String.class);
- * 
- * String result = response.getBody();
- *
- * 
- * System.out.println("result: " + result);
- * }
- * return "Student Added";
- * }
- * 
- * }
- * 
- */
